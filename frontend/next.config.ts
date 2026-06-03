a87e4026c0052dffcc8a7aecff5c07281dad75f0
bogomollov <bogdan.bogomolov.2016@mail.ru>
2026-06-03 22:03:51 +0500

    fix: turbopack multiple lockfiles warning

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   experimental: {
@@ -7,6 +8,9 @@ const nextConfig: NextConfig = {
   images: {
     loader: "custom",
     loaderFile: "./lib/imageLoader.ts",
+  },
+  turbopack: {
+    root: path.resolve(__dirname, '..'),
   },
 };
 
